b061c3b21e1ca65a215ed8806cd33ce56ab29f24
Brandon Carter <brandon.d.carter99@gmail.com>
2021-06-14 15:55:03 -0400

    Created the __init__ function which accepts a variable number of arguments in the form of color=number, splits them, and adds the color in string form to the contents list 'number' amount of times

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,8 +2,12 @@ import copy
 import random
 # Consider using the modules imported above.
 
-class Hat:
-  pass
+class Hat():
+    def __init__(self, **kwargs):
+        contents = []
+        for key, value in kwargs.items():
+            for color in range(value):
+                contents.append(str(key))
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
