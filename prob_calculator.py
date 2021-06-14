7b21dcd4a97c0cdeaa107cac8b730802bec4bd3b
Brandon Carter <brandon.d.carter99@gmail.com>
2021-06-14 17:02:04 -0400

    Added draw function, which, currently, accepts a number for the number of draws to make, then randomly selects and removes an element from the contents list and places it in the drawn_balls list. If the number to draw is larger than the length of contents then nothing happens

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,10 +4,30 @@ import random
 
 class Hat():
     def __init__(self, **kwargs):
-        contents = []
+        self.contents = []
         for key, value in kwargs.items():
             for color in range(value):
-                contents.append(str(key))
+                self.contents.append(str(key))
+
+    def draw(self, num_to_draw):
+        '''
+        Removes balls at random from contents and returns those balls as a list of strings. The balls should
+        not return to the hat during the draw. If the number of balls to draw exceeds the available quantity,
+        return all the balls
+        '''
+        # if number of balls > len(contents) do nothing, print('Not enough balls')
+        if len(self.contents) <= num_to_draw:
+            drawn_balls = []
+            # use copy import to make a copy of the hat in order to pull balls out w/o effecting the original hat
+            content_copy = copy.deepcopy(self.contents)
+            # use random import to remove a random item from the list, adding it to another list
+            for i in range(num_to_draw):
+                ball = random.choice(self.contents)
+                drawn_balls.append(ball)
+                self.contents.remove(ball)
+            print(self.contents, drawn_balls)
+        else:
+            print('Not enough balls')
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
