e80bbd926d7031511c5e901f942bc466df0d7ff0
Brandon Carter <brandon.d.carter99@gmail.com>
2021-06-15 15:01:39 -0400

    Created the main function of the experiment function, however it is giving the wrong probability in the end.

--- prob_calculator.py
+++ prob_calculator.py
@@ -15,25 +15,48 @@ class Hat():
         not return to the hat during the draw. If the number of balls to draw exceeds the available quantity,
         return all the balls
         '''
-        # if number of balls > len(contents) do nothing, print('Not enough balls')
-        if len(self.contents) <= num_to_draw:
-            drawn_balls = []
-            # use copy import to make a copy of the hat in order to pull balls out of original list and being
-            # able to restore it later
-            content_copy = copy.deepcopy(self.contents)
+        drawn_balls = []
+        if len(self.contents) > num_to_draw:
             # use random import to remove a random item from the list, adding it to another list
             for i in range(num_to_draw):
                 ball = random.choice(self.contents)
                 drawn_balls.append(ball)
                 self.contents.remove(ball)
-            self.contents = content_copy
-            return drawn_balls
         else:
             print('Not enough balls')
+        return drawn_balls
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    '''
+    Returns the probability of a given set of colors being picked from the original set
+    '''
     ideal_balls = []
+    # creates a list similar to self.contents, in order to compare the two
     for key, value in expected_balls.items():
         for color in range(value):
             ideal_balls.append(str(key))
+
+    guessed_right = 0
+    expected_balls_copy = copy.deepcopy(expected_balls)
+    for i in range(num_experiments):
+        # draw x amount of times
+        drawn_balls = hat.draw(num_balls_drawn)
+        # compare drawn_balls to expected balls. If element in drawn_balls in expected balls, remove from drawn_balls
+        for color in range(num_balls_drawn):
+            try:
+                if drawn_balls[color] in expected_balls:
+                    expected_balls.pop(drawn_balls[color])
+                else:
+                    pass
+            except:
+                pass
+        # if expected_balls empty, +1 for how many times it was equal
+        if expected_balls == []:
+            guessed_right += 1
+            expected_balls = expected_balls_copy
+        else:
+            expected_balls = expected_balls_copy
+    # estimate probability by number of times guess was correct / number of experiments
+    probability = guessed_right / num_experiments
+    return probability
